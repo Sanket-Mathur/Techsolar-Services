cca8422990c4908a20d90f6e5292f2c5ce70b5fe
Sanket Mathur <rajeev.sanket@gmail.com>
2020-06-28 21:11:56 +0530

    Day 1 Build

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function () {
+    
+    $('#navbar-button').blur(function (event) {
+        
+        var screenWidth = window.innerWidth;
+        if(screenWidth < 768) {
+            $('#collapsable-nav').collapse('hide');
+        }
+        
+    });
+    
+});
