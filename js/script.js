663e5ecc94abdedcbe04606798da156ad7a52356
Sanket Mathur <rajeev.sanket@gmail.com>
2020-07-02 21:23:22 +0530

    Day 5 Build

--- js/script.js
+++ js/script.js
@@ -17,6 +17,9 @@ $(function () {
 
 	var homeHtml = "snippets/home.html";
 	var contactHtml = "snippets/contact.html";
+    var projectHtml = "snippets/projects.html";
+    var servicesHtml = "snippets/services.html";
+    var aboutHtml = "snippets/about.html";
 
 	var insertHtml = function (selector, html) {
 		var targetElem = document.querySelector(selector);
@@ -28,10 +31,19 @@ $(function () {
  		html += "<img src='images/ajax-loader.gif'></div>";
   		insertHtml(selector, html);
 	};
+    
+    var removeActive = function () {
+        document.querySelector("#n-hom").removeAttribute("class")
+        document.querySelector("#n-ser").removeAttribute("class")
+        document.querySelector("#n-pro").removeAttribute("class")
+        document.querySelector("#n-abo").removeAttribute("class")
+        document.querySelector("#n-con").removeAttribute("class")
+    }
 
     document.addEventListener("DOMContentLoaded", function (event) {
 
         showLoading("#main");
+        document.querySelector("#n-hom").setAttribute("class", "active")
         $ajaxUtils.sendGetRequest(
             homeHtml,
             function (responseText) {
@@ -43,6 +55,8 @@ $(function () {
 
     ts.loadContact = function () {
         showLoading("#main");
+        removeActive();
+        document.querySelector("#n-con").setAttribute("class", "active")
         $ajaxUtils.sendGetRequest(
             contactHtml,
             function (responseText) {
@@ -54,8 +68,49 @@ $(function () {
     
     ts.loadHome = function () {
         showLoading("#main");
+        removeActive();
+        document.querySelector("#n-hom").setAttribute("class", "active")
         $ajaxUtils.sendGetRequest(
             homeHtml,
+            function (responseText) {
+                document.querySelector("#main").innerHTML = responseText;
+            },
+            false
+        );
+    }
+    
+    ts.loadServices = function () {
+        showLoading("#main");
+        removeActive();
+        document.querySelector("#n-ser").setAttribute("class", "active")
+        $ajaxUtils.sendGetRequest(
+            servicesHtml,
+            function (responseText) {
+                document.querySelector("#main").innerHTML = responseText;
+            },
+            false
+        );
+    }
+    
+    ts.loadAbout = function () {
+        showLoading("#main");
+        removeActive();
+        document.querySelector("#n-abo").setAttribute("class", "active")
+        $ajaxUtils.sendGetRequest(
+            aboutHtml,
+            function (responseText) {
+                document.querySelector("#main").innerHTML = responseText;
+            },
+            false
+        );
+    }
+    
+    ts.loadProjects = function () {
+        showLoading("#main");
+        removeActive();
+        document.querySelector("#n-pro").setAttribute("class", "active")
+        $ajaxUtils.sendGetRequest(
+            projectHtml,
             function (responseText) {
                 document.querySelector("#main").innerHTML = responseText;
             },
